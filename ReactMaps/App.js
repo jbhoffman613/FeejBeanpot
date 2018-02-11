c812ed2995a5606891729491fc0eadb41041c78b
jbhoffman613 <jbhoffman613@gmail.com>
2018-02-11 08:22:46 -0500

    Tested the map drag features

--- ReactMaps/App.js
+++ ReactMaps/App.js
@@ -26,39 +26,55 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  annotationContainer: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 15,
+  },
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'orange',
+    transform: [{ scale: 0.6 }],
+  }
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  renderAnnotations () {
+    return (
+      <Mapbox.PointAnnotation
+        key='pointAnnotation'
+        id='pointAnnotation'
+        coordinate={[42.342362, -71.062868]}>
+
+        <View style={styles.annotationContainer}>
+          <View style={styles.annotationFill} />
+        </View>
+        <Mapbox.Callout title='Look! An annotation!' />
+      </Mapbox.PointAnnotation>
+    )
+  }
+
+  //Renders the image and and all of its Components onto the string
   render() {
     return (
       <View style={styles.container}>
         <Mapbox.MapView
-            styleURL={Mapbox.StyleURL.Street}
+            styleURL={Mapbox.StyleURL.Dark}
             zoomLevel={15}
-            centerCoordinate={[11.256, 43.770]}
-            style={styles.container}>
+            centerCoordinate={[42.342362, -71.062855]}
+            style={styles.container}
+            showUserLocation={true}>
+            {this.renderAnnotations()}
         </Mapbox.MapView>
       </View>
     );
   }
-}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
+}
